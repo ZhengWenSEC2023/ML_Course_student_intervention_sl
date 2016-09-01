b7e84387777f5de489ee5bbfcdb60f45c441165a
shangliy <kudoysl@gmail.com>
2016-09-01 12:02:53 -0700

    First Update

---
+++ st_inter_sl.py
@@ -0,0 +1,28 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+from time import time
+from sklearn.metrics import f1_score
+
+# Read student data
+student_data = pd.read_csv("student-data.csv")
+print "Student data read successfully!"
+
+# Extract feature columns
+feature_cols = list(student_data.columns[:-1])
+
+# Extract target column 'passed'
+target_col = student_data.columns[-1] 
+
+# Show the list of columns
+print "Feature columns:\n{}".format(feature_cols)
+print "\nTarget column: {}".format(target_col)
+
+# Separate the data into feature data and target data (X_all and y_all, respectively)
+X_all = student_data[feature_cols]
+y_all = student_data[target_col]
+
+# Show the feature information by printing the first five rows
+print "\nFeature values:"
+print X_all.head()
+
